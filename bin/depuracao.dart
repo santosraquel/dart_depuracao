235c967166c9c6978af1ecf4c43d161e9fcba676
santosraquel <raquel.dossantos1965@gmail.com>
2022-05-13 19:37:10 -0300

    criando novo projeto para depuração

---
+++ bin/depuracao.dart
@@ -0,0 +1,7 @@
+import 'package:depuracao/depuracao.dart' as dart_depuracao;
+import 'package:depuracao/a_depuracao.dart' as a_depuracao_aprovacao;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_depuracao.calculate()}!');
+  a_depuracao_aprovacao.show();
+}
